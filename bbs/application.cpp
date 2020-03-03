617d440514de32fd78ff0b38de4cfd82bbf6066f
Andrew Baker <abaker@barracuda.com>
2020-03-02 20:24:48 -0500

    [undefined] => Gt -p

--- bbs/application.cpp
+++ bbs/application.cpp
@@ -760,7 +760,18 @@ int Application::Run(int argc, char* argv[]) {
     cout << "WWIV Bulletin Board System [" << wwiv_version << beta_version << "]\r\n\n";
     return 0;
   } else  if (cmdline.barg("nodestatus")) {
-    multi_instance();
+    auto num = num_instances();
+    if (num < 1) {
+      bout << "|#6Couldn't find instance data file.\r\n";
+      return;
+    }
+
+    bout << fmt::sprintf("|#5Node |#1%-35.35s |#2%-37.37s\r\n", "User Name", "Activity");
+    bout << "==== " << string(35, '=') << " " << string(37, '=') << "\r\n";
+
+    for (int inst = 1; inst <= num; inst++) {
+      bout << make_inst_str(inst, INST_FORMAT_LIST) << "\r\n";
+    }
     return 0;
   }
 
